526eac22954a5683f2ef0c2004e0fd400af5b4f9
iyear <ljyngup@gmail.com>
2023-02-28 16:23:52 +0800

    feat(env): add ruby, rust, rapidjson, dotnet, funchook

--- pkg/builder/env.go
+++ pkg/builder/env.go
@@ -2,6 +2,7 @@ package builder
 
 import (
 	_ "embed"
+	"fmt"
 	"github.com/moby/buildkit/client/llb"
 	"runtime"
 )
@@ -9,14 +10,14 @@ import (
 /*
 // languages
 -[x] python
--[ ] ruby
--[ ] rust
--[ ] netcore
+-[x] ruby
+-[x] rust
+-[x] netcore
 -[ ] netcore2
 -[ ] netcore5
 -[ ] netcore7
--[ ] rapidjson
--[ ] funchook
+-[x] rapidjson
+-[x] funchook
 -[ ] v8/v8rep54
 -[ ] v8rep57
 -[ ] v8rep58
@@ -40,11 +41,6 @@ import (
 -[ ] clangformat
 -[x] backtrace
 */
-
-var languageEnvs = map[string]func(llb.State) llb.State{
-	"python": pythonEnv,
-	// TODO(iyear): Add more languages
-}
 
 type Env struct {
 	state llb.State
@@ -123,21 +119,72 @@ func (e Env) Backtrace() Env {
 	return e
 }
 
-func (e Env) Languages(languages []string) Env {
-	for _, lang := range languages {
-		if env, ok := languageEnvs[lang]; ok {
-			e.state = env(e.state)
-		}
-	}
+func (e Env) Python() Env {
+	e.state = e.state.Run(llb.Shlex("apt-get update")).
+		Run(llb.Shlex("apt-get -y --no-install-recommends install python3 python3-dev python3-pip")).
+		Root()
+
+	return e
+}
+
+func (e Env) Ruby() Env {
+	e.state = e.state.Run(llb.Shlex("apt-get update")).
+		Run(llb.Shlex("apt-get install -y --no-install-recommends ruby ruby-dev")).
+		Root()
+
+	return e
+}
+
+func (e Env) Rust() Env {
+	e.state = e.state.Run(llb.Shlex("apt-get install -y --no-install-recommends curl")).
+		Run(llb.Shlex("curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain nightly-2021-12-04 --profile default")).
+		Root()
+
+	return e
+}
+
+func (e Env) RapidJSON() Env {
+	e.state = e.state.Run(llb.Shlex("git clone https://github.com/miloyip/rapidjson.git")).
+		Dir("rapidjson").
+		Run(llb.Shlex("git checkout v1.1.0")).
+		Run(llb.Shlex("mkdir build")).
+		Dir("build").
+		Run(llb.Shlex("cmake -DRAPIDJSON_BUILD_DOC=Off -DRAPIDJSON_BUILD_EXAMPLES=Off -DRAPIDJSON_BUILD_TESTS=Off ..")).
+		Run(llb.Shlex("make && make install")).
+		Dir("../..").
+		Run(llb.Shlex("rm -rf ./rapidjson")).
+		Root()
+
+	return e
+}
+
+func (e Env) FuncHook() Env {
+	e.state = e.state.Run(llb.Shlex("apt-get update")).
+		Run(llb.Shlex("apt-get install -y --no-install-recommends cmake")).
+		Root()
+
+	return e
+}
+
+func (e Env) NetCore() Env {
+	version := "1.1.11"
+	e.state = e.state.Run(llb.Shlex("apt-get install -y --no-install-recommends libc6 libcurl3 libgcc1 libgssapi-krb5-2 libicu57 liblttng-ust0 libssl1.0.2 libstdc++6 libunwind8 libuuid1 zlib1g")).
+		Run(llb.Shlexf("wget %s -O dotnet.tar.gz", fmt.Sprintf("https://dotnetcli.blob.core.windows.net/dotnet/Sdk/%s/dotnet-dev-debian.9-x64.%s.tar.gz", version, version))).
+		Run(llb.Shlex("mkdir -p /usr/share/dotnet")).
+		Run(llb.Shlex("tar -zxf dotnet.tar.gz -C /usr/share/dotnet")).
+		Run(llb.Shlex("rm dotnet.tar.gz")).
+		Run(llb.Shlex("ln -s /usr/share/dotnet/dotnet /usr/bin/dotnet")).
+		Run(llb.Shlex("mkdir warmup")).
+		Dir("warmup").
+		Run(llb.Shlex("dotnet new")).
+		Dir("..").
+		Run(llb.Shlex("rm -rf warmup")).
+		Run(llb.Shlex("rm -rf /tmp/NuGetScratch")).
+		Root()
+
 	return e
 }
 
 func (e Env) Root() llb.State {
 	return e.state
-}
-
-func pythonEnv(base llb.State) llb.State {
-	return base.
-		Run(llb.Shlex("apt-get update")).
-		Run(llb.Shlex("apt-get -y --no-install-recommends install python3 python3-dev python3-pip")).Root()
 }
